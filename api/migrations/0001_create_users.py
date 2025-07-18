0b2586c82174e07cc0c75fe817321973d9d7fbfe
Nikorgl <nng2906@gmail.com>
2025-07-18 20:29:08 +0700

    -

--- api/migrations/0001_create_users.py
+++ api/migrations/0001_create_users.py
@@ -5,10 +5,10 @@ from django.contrib.auth import get_user_model
 def create_users(apps, schema_editor):
     User = get_user_model()
     User.objects.create_superuser(username='adm' , password='Admiragl29')
-    User.objects.create_user(username='seva'     , password='Sevargl6'    , first_name='Сева')
-    User.objects.create_user(username='egor'     , password='Georgl9'     , first_name='Егор')
-    User.objects.create_user(username='vara'     , password='Varvargl13'  , first_name='Варя')
-    User.objects.create_user(username='klim'     , password='Kremengl20'  , first_name='Клим')
+    User.objects.create_user(username='seva'     , password='seva'    , first_name='Сева')
+    User.objects.create_user(username='egor'     , password='egor'     , first_name='Егор')
+    User.objects.create_user(username='vara'     , password='vara'  , first_name='Варя')
+    User.objects.create_user(username='klim'     , password='klim'  , first_name='Клим')
     User.objects.create_user(username='alena'    , password='alena'       , first_name='Alena')
     User.objects.create_user(username='anna'     , password='anna'     , first_name='Аня')
 
